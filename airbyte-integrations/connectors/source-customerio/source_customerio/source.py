2158166350a79dd53d32898d611d9057da059205
denimyftiu <deni2myftiu@gmail.com>
2021-10-18 21:00:52 +0200

    initial setup for customer io airbyre source

---
+++ airbyte-integrations/connectors/source-customerio/source_customerio/source.py
@@ -0,0 +1,45 @@
+#
+# Copyright (c) 2021 Airbyte, Inc., all rights reserved.
+#
+from abc import ABC
+from typing import Any, List, Mapping, Tuple
+
+from airbyte_cdk.models import SyncMode
+from airbyte_cdk.sources import AbstractSource
+from airbyte_cdk.sources.streams import Stream
+from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
+
+from .streams import (
+    Activities, Broadcasts, BroadcastMetrics, BroadcastMetricsLinks
+)
+
+# Source
+class SourceCustomerio(AbstractSource):
+
+    def check_connection(self, logger, config) -> Tuple[bool, any]:
+        try:
+            authenticator = TokenAuthenticator(token=config["app_api_key"])
+            test_stream = Activities(authenticator=authenticator,
+                                     region=config['region'])
+            gen = test_stream.read_records(sync_mode=SyncMode.full_refresh)
+            next(gen)
+            return True, None
+        except Exception as e:
+            # print(e)
+            return False, CONNECTION_ERROR.format(e)
+
+    def streams(self, config: Mapping[str, Any]) -> List[Stream]:
+        authenticator = TokenAuthenticator(token=config["app_api_key"])
+        args = {"authenticator": authenticator, "region": config["region"]}
+        return [
+            Activities(**args),
+            Broadcasts(**args),
+            BroadcastMetrics(**args),
+            BroadcastMetricsLinks(**args),
+        ]
+
+CONNECTION_ERROR = '''Connection check failed with error: {}.
+Check if the workers IP address is whitelisted in the CustomerIO
+console or if acces token is set up correctly.
+Also you may have configured the wrong region.'''
+
---
+++ airbyte-integrations/connectors/source-customerio/source_customerio/streams.py
@@ -0,0 +1,205 @@
+#
+# Copyright (c) 2021 Airbyte, Inc., all rights reserved.
+#
+import requests
+from abc import ABC, abstractmethod
+from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
+
+from airbyte_cdk.models import SyncMode
+from airbyte_cdk.sources.streams.http import HttpStream
+
+# Basic full refresh stream
+class CustomerioStream(HttpStream):
+    url_base = ''
+
+    def __init__(self, region: str, **kwargs):
+        super().__init__(**kwargs)
+        self.region = region
+        if region == 'US':
+            self.url_base = 'https://beta-api.customer.io/v1/api/'
+        elif region == 'EU':
+            self.url_base = 'https://beta-api-eu.customer.io/v1/api/'
+        else:
+            raise ValueError('Region must me set to US or EU.')
+
+# Basic incremental stream
+class IncrementalCustomerioStream(CustomerioStream, ABC):
+    state_checkpoint_interval = None
+
+    @property
+    def cursor_field(self) -> str:
+        pass
+
+    def get_updated_state(
+        self,
+        current_stream_state: MutableMapping[str, Any],
+        latest_record: Mapping[str, Any]
+    ) -> Mapping[str, Any]:
+        pass
+
+
+class Activities(CustomerioStream):
+    '''
+    API docs: https://customer.io/docs/api/#operation/listActivities
+    Returns all activities of different types.
+    We get all event types for activities.
+    '''
+    primary_key = 'id'
+
+    def path(self, **kwargs):
+        return 'activities'
+
+    def next_page_token(
+        self,
+        response: requests.Response
+    ) -> Optional[Mapping[str, Any]]:
+        next_page_token = response.json()
+
+        token = next_page_token.get('next')
+        if token:
+            return {'start': token}
+        return {}
+                 
+
+    def request_params(
+        self,
+        stream_state: Mapping[str, Any],
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None,
+    ) -> MutableMapping[str, Any]:
+
+        # CustomerIO default pagination is 10, max is 100
+        # (TODO: denimyftiu) change the limit before you commit.
+        params = {"limit": 10}
+
+        if next_page_token:
+            params.update(next_page_token)
+
+        return params
+
+    def parse_response(
+        self,
+        response: requests.Response,
+        **kwargs
+    ) -> Iterable[Mapping]:
+        response_json = response.json()
+        yield from response_json.get('activities', [])
+
+
+class Broadcasts(CustomerioStream):
+    '''
+    API docs: https://customer.io/docs/api/#operation/listBroadcasts 
+    Returns all activities of different types.
+    We get all event types for activities.
+    '''
+    name = 'broadcasts'
+    primary_key = 'id'
+
+    def path(self, **kwargs):
+        return 'broadcasts'
+
+    def next_page_token(
+        self,
+        response: requests.Response
+    ) -> Optional[Mapping[str, Any]]:
+        pass
+
+    def parse_response(
+        self,
+        response: requests.Response,
+        **kwargs
+    ) -> Iterable[Mapping]:
+        response_json = response.json()
+        yield from response_json.get('broadcasts', [])
+
+class BroadcastMetrics(CustomerioStream):
+    """
+    API docs: https://customer.io/docs/api/#operation/broadcastMetrics
+    """
+
+    name = "broadcast_metrics"
+    primary_key = 'id'
+
+    def path(self, stream_slice: Mapping[str, Any] = None, **kwargs):
+        broadcast_id = stream_slice["broadcast_id"]
+        return f"broadcasts/{broadcast_id}/metrics"
+
+    def next_page_token(
+        self,
+        response: requests.Response
+    ) -> Optional[Mapping[str, Any]]:
+        return None
+
+    def parse_response(
+        self,
+        response: requests.Response,
+        **kwargs
+    ) -> Iterable[Mapping]:
+        yield response.json()
+
+    def read_records(self, stream_slice: Optional[Mapping[str, Any]] = None, **kwargs) -> Iterable[Mapping[str, Any]]:
+        broadcasts_stream = Broadcasts(authenticator=self.authenticator, region=self.region)
+        for broadcast in broadcasts_stream.read_records(sync_mode=SyncMode.full_refresh):
+            yield from super().read_records(stream_slice={"broadcast_id": broadcast["id"]}, **kwargs)
+
+
+class BroadcastMetricsLinks(CustomerioStream):
+    """
+    API docs: https://customer.io/docs/api/#operation/broadcastLinks
+    """
+
+    name = "broadcast_metrics_links"
+    primary_key = 'id'
+
+    def path(self, stream_slice: Mapping[str, Any] = None, **kwargs):
+        broadcast_id = stream_slice["broadcast_id"]
+        return f"broadcasts/{broadcast_id}/metrics/links"
+
+    def next_page_token(
+        self,
+        response: requests.Response
+    ) -> Optional[Mapping[str, Any]]:
+        return None
+
+    def parse_response(
+        self,
+        response: requests.Response,
+        **kwargs
+    ) -> Iterable[Mapping]:
+        yield response.json()
+
+    def read_records(self, stream_slice: Optional[Mapping[str, Any]] = None, **kwargs) -> Iterable[Mapping[str, Any]]:
+        broadcasts_stream = Broadcasts(authenticator=self.authenticator, region=self.region)
+        for broadcast in broadcasts_stream.read_records(sync_mode=SyncMode.full_refresh):
+            yield from super().read_records(stream_slice={"broadcast_id": broadcast["id"]}, **kwargs)
+
+
+# class BroadcastActions(CustomerioStream):
+#     """
+#     API docs: https://customer.io/docs/api/#operation/broadcastActions
+#     """
+# 
+#     name = "broadcast_actions"
+#     primary_key = 'id'
+# 
+#     def path(self, stream_slice: Mapping[str, Any] = None, **kwargs):
+#         broadcast_id = stream_slice["broadcast_id"]
+#         return f"broadcasts/{broadcast_id}/actions"
+# 
+#     def next_page_token(
+#         self,
+#         response: requests.Response
+#     ) -> Optional[Mapping[str, Any]]:
+#         return None
+# 
+#     def parse_response(
+#         self,
+#         response: requests.Response,
+#         **kwargs
+#     ) -> Iterable[Mapping]:
+#         yield response.json()
+# 
+#     def read_records(self, stream_slice: Optional[Mapping[str, Any]] = None, **kwargs) -> Iterable[Mapping[str, Any]]:
+#         broadcasts_stream = Broadcasts(authenticator=self.authenticator, region=self.region)
+#         for broadcast in broadcasts_stream.read_records(sync_mode=SyncMode.full_refresh):
+#             yield from super().read_records(stream_slice={"broadcast_id": broadcast["id"]}, **kwargs)
